6247a1a1a87f7364420319ed1d3a97f6c860c089
Fer Sánchez <sangarfer@gmail.com>
2022-03-01 12:48:29 +0100

    list class done

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 // An array containing HTML elements
-const numbersList = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
+const numbersList = [<li key={"1"}>1</li>, <li key={"2"}>2</li>, <li key={"3"}>3</li>, <li key={"4"}>4</li>];
 
 function List() {
   return (
